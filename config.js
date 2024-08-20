e081b7f0a0005e4668f942fb3a07aaf09bc6e819
Pinless11 <batrand438@gmail.com>
2024-08-20 21:28:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
-//Bot by 𝕶𝖎𝖓𝖌 𝕾𝖆𝖒//
+//Bot by Pinless//
 
 
 require("./all/module")
-global.owner = "2348024322741" 
-global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.owner = "254746891290" 
+global.namaCreator = "Pinless" 
 global.autoJoin = false 
 global.antilink = false 
 global.versisc = '6.0' 
-global.codeInvite = "https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e"
+global.codeInvite = "https://whatsapp.com/channe/0029VaaqaSp0LKZDuwe5SI3e"
 global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.packname = "Pinless" 
 global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
 global.jumlah = "5" 
 global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
